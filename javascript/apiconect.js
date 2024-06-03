79adcb3bb20ee0f77dfefa98197437df3e28dd0e
Rodrigo-RRD <rodrigorrd31@gmail.com>
2024-06-02 23:59:50 -0300

    Organizando e refatorando pagina, css, js e svg

--- javascript/apiconect.js
+++ javascript/apiconect.js
@@ -1,37 +1,84 @@
+
 $(document).ready(function () {
-    consultarProjetoAPI();
+    var page = 0;
+    consultarProjetoAPI(page);
 });
 
-function consultarProjetoAPI() {
+function consultarProjetoAPI(page) {
 
     $.ajax({
         url: "http://localhost:8080/v1/projetos/listar",
         type: "get",
+        data: {
+            page: page,
+            size: 3
+        },
         success: function (response) {
             criarLitaProjetos(response);
+        },error: function(jqXHR, textStatus, errorThrown) {
+            // Aqui você pode lidar com o erro da maneira que preferir
+            console.error("Erro na requisição AJAX:", textStatus, errorThrown);
         }
     });
 }
 
 function criarLitaProjetos(response) {
+    var projetos = response.content;
 
-    var projetos = response;
+    $(".containerCards").remove();
+    $("#currentUsuario").text(response.number + 1);
+    $("#totalPages").text(response.totalPages);
+    $("#currentPage").text(response.number);
 
     $.each(projetos, function (b, projeto) {
 
-        var cards = $("<div>").addClass("cardN" + b).append(
-            $("<div>").addClass("card-body").append(
-                $("<img>").addClass("img-card" + b).attr({
-                    src: "/img/javaImage.png",
-                    alt: "Image description"
-                }),
-                $("<div>").addClass("textCard").append(
-                    $("<h5>").addClass("textAreaConhecimento").text(projeto.areaConhecimento).append(
-                        $("<p>").addClass("card-text").text("Tema: " + projeto.tema),
-                        $("<p>").addClass("card-text").text("Status: " + projeto.status.toLowerCase())
-                    )))
-        );
+        var cards =
+            $("<div>").addClass("containerCards").append(
+                $("<div>").addClass("cardN" + b).append(
+                    $("<div>").addClass("card-body").append(
+                        $("<img>").addClass("img-card" + b).attr({
+                            src: "/img/javaImage.png",
+                            alt: "Image description"
+                        }),
+                        $("<div>").addClass("textCard").append(
+                            $("<h5>").addClass("textAreaConhecimento").text(projeto.areaConhecimento).append(
+                                $("<p>").addClass("card-text").text("Tema: " + projeto.tema),
+                                $("<p>").addClass("card-text").text("Status: " + projeto.status.toLowerCase())
+                            )))
+                ));
+
         cards.appendTo(".criarCards");
+
+        // Paginação
+        var totalPages = response.totalPages; // Número total de páginas
+        var page = response.number;
+        var elementN = response.numberOfElements;
+
+
+        if (page < totalPages - 1) {
+            $('#btn-next').prop('disabled', false); // Habilita o botão de próxima página
+        } else {
+            $('#btn-next').prop('disabled', true); // Desabilita o botão de próxima página
+        }
+        if (page > 0) {
+            $('#btn-prev').prop('disabled', false); // Habilita o botão de página anterior
+        } else {
+            $('#btn-prev').prop('disabled', true); // Desabilita o botão de página anterior
+        }
+        if (elementN === 0) {
+            $('#btn-next').prop('disabled', true); // Desabilita o botão de próxima página
+            $('#btn-prev').prop('disabled', true); // Desabilita o botão de página anterior
+        }
     });
 }
 
+
+$('#btn-next').click(function () {
+    var currentPage = parseInt($("#currentPage").text());
+    consultarProjetoAPI(currentPage + 1);
+});
+
+$('#btn-prev').click(function () {
+    var currentPage = parseInt($('#currentPage').text());
+    consultarProjetoAPI(currentPage - 1);
+});
--- javascript/upload.js
+++ javascript/upload.js
@@ -1,16 +1,21 @@
-$(document).ready(function() {
 
-//======= Funções e eventos relacionados à inserção do arquivo =======//
-    $('#btnSelArquivo').on('click', function(event) {
+$(document).ready(function () {
+
+    // https://www.svgrepo.com
+    var pdfIconPath = 'icons/extensaoarquivos/pdf.svg';
+    var wordIconPath = 'icons/extensaoarquivos/doc.svg';
+
+    //======= Funções e eventos relacionados à inserção do arquivo =======//
+    $('#btnSelArquivo').on('click', function (event) {
         event.preventDefault();
         $('#fileInput').click();
     });
 
-    $('#fileInput').on('change', function(event) {
-        if(event.target.files[0]){
+    $('#fileInput').on('change', function (event) {
+        if (event.target.files[0]) {
             let extensao = event.target.files[0].name.split('.').pop().toLowerCase();
 
-            if(extensao !== 'pdf' && extensao !== 'doc' && extensao !== 'docx'){
+            if (extensao !== 'pdf' && extensao !== 'doc' && extensao !== 'docx') {
                 return;
             }
         }
@@ -18,28 +23,28 @@ $(document).ready(function() {
         handleFiles(event.target.files);
     });
 
-    $('#dropBox').on('dragover', function(event) {
+    $('#dropBox').on('dragover', function (event) {
         event.preventDefault();
         event.stopPropagation();
         $(this).addClass('dragover');
     });
 
-    $('#dropBox').on('dragleave', function(event) {
+    $('#dropBox').on('dragleave', function (event) {
         event.preventDefault();
         event.stopPropagation();
         $(this).removeClass('dragover');
     });
 
-    $('#dropBox').on('drop', function(event) {
+    $('#dropBox').on('drop', function (event) {
         event.preventDefault();
         event.stopPropagation();
         $(this).removeClass('dragover');
         const files = event.originalEvent.dataTransfer.files;
 
-        if(files){
+        if (files) {
             let extensao = files[0].name.split('.').pop().toLowerCase();
 
-            if(extensao !== 'pdf' && extensao !== 'doc' && extensao !== 'docx'){
+            if (extensao !== 'pdf' && extensao !== 'doc' && extensao !== 'docx') {
                 return;
             }
         }
@@ -48,72 +53,96 @@ $(document).ready(function() {
         handleFiles(files);
     });
 
+    // Função para mostrar ou ocultar o botão de cancelar
+    function toggleCancelarButton(show) {
+        if (show) {
+            $('#btnCancelar').show();
+        } else {
+            $('#btnCancelar').hide();
+        }
+    }
     function handleFiles(files) {
         const file = files[0];
         if (file) {
             const fileName = file.name;
+            const minFileName = fileName.substring(0, 25);
             const fileSize = (file.size / (1024 * 1024)).toFixed(2) + 'MB';
             const fileExtension = fileName.split('.').pop().toLowerCase();
 
             if (fileExtension === 'pdf') {
-                $('#file-icon').attr("src","/img/pdf_icon.svg");
+                $('#file-icon').attr("src", pdfIconPath);
             } else if (fileExtension === 'doc' || fileExtension === 'docx') {
-                $('#file-icon').attr("src","/img/docx_icon.svg");
+                $('#file-icon').attr("src", wordIconPath);
             }
 
-            $('.file-name').text(fileName);
+            $('.file-name').text(minFileName + "...");
             $('.file-size').text(fileSize);
             $('#fileDetails').show();
-            $('#fileDetails').css('display', 'flex');;
+            $('#fileDetails').css('display', 'flex');
+
+            toggleCancelarButton(true); // Mostra o botão de cancelar
         }
     }
-//=================================================================================//
+    //=================================================================================//
 
-//======= Funções e eventos relacionados ao envio das informações para a API =======//
+    //======= Funções e eventos relacionados ao envio das informações para a API =======//
 
-$('#btnEnviar').on('click', function(event) {
-    event.preventDefault();
-    const file = $('#fileInput')[0].files[0];
-    if (file) {
-        const formData = new FormData();
+    $('#btnEnviar').on('click', function (event) {
+        event.preventDefault();
+        const file = $('#fileInput')[0].files[0];
+        if (file) {
+            const formData = new FormData();
 
-        // Formatar a data no formato dd/MM/yyyy HH:mm:ss
-        const now = new Date();
-        const dataFormatada = now.toLocaleString('pt-BR', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit'
-        }).replace(',', '');
+            // Formatar a data no formato dd/MM/yyyy HH:mm:ss
+            const now = new Date();
+            const dataFormatada = now.toLocaleString('pt-BR', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            }).replace(',', '');
 
-        formData.append('arquivo', file);
-        formData.append('nome', $('#titulo_projeto').val());
-        formData.append('dataCriacao', dataFormatada);
-        formData.append('resumo', $('#descricao_projeto').val());
+            formData.append('arquivo', file);
+            formData.append('nome', $('#titulo_projeto').val());
+            formData.append('dataCriacao', dataFormatada);
+            formData.append('resumo', $('#descricao_projeto').val());
 
-        $.ajax({
-            url: 'http://localhost:8080/v1/documentos/salvar2',
-            type: 'POST',
-            data: formData,
-            processData: false,
-            contentType: false,
-            success: function(response) {
-                alert('Arquivo enviado com sucesso!');
+            $.ajax({
+                url: 'http://localhost:8080/v1/documentos/salvar2',
+                type: 'POST',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function (response) {
+                    alert('Arquivo enviado com sucesso!');
 
-                // Redirecionando
-                window.location.replace("http://localhost:8080/v1/documentos");
-            },
-            error: function(xhr, status, error) {
-                alert('Erro ao enviar o arquivo.');
-                console.error(xhr, status, error);
-            }
-        });
-    } else {
-        alert('Por favor, selecione um arquivo primeiro.');
-    }
-});
+                    // Redirecionando
+                    window.location.replace("http://localhost:8080/v1/documentos");
+                },
+                error: function (xhr, status, error) {
+                    alert('Erro ao enviar o arquivo.');
+                    console.error(xhr, status, error);
+                }
+            });
+        } else {
+            alert('Por favor, selecione um arquivo primeiro.');
+        }
+    });
 
-//==================================================================================//
+    //==================================================================================//
+    // Cancelar upload
+    $('#btnCancelar').on('click', function (event) {
+        event.preventDefault();
+        $('#fileInput').val('');
+        $('#fileDetails').hide();
+        toggleCancelarButton(false); // Oculta o botão de cancelar
+    });
+
+    // Sair da página
+    $('#btnSair').on('click', function (event) {
+        event.preventDefault();
+        window.location.href = 'trabalhos.html'; // Substitua 'pagina_anterior.html' pelo URL desejado.
+    });
 });
